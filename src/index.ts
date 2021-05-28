e9b1dec080c74e4aa81bfc122d7ac4627b8ac37c
André Dietrich <andredietrich@web.de>
2021-05-28 14:45:11 +0200

    Update to LiaScript 0.9.18

--- src/index.ts
+++ src/index.ts
@@ -30,7 +30,7 @@ function liascript() {
 
 if (argv.v || argv.version) {
   console.log("DevServer: 1.0.5");
-  console.log("LiaScript: 0.9.17");
+  console.log("LiaScript: 0.9.18");
   process.exit();
 }
 
