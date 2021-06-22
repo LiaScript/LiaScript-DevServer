7e326fe82d15afd032ab42d16c84927e7c22b5a5
André Dietrich <andredietrich@web.de>
2021-06-22 11:06:28 +0200

    Upgrade to LiaScript 0.9.20

--- README.md
+++ README.md
@@ -79,7 +79,7 @@ for LiaScript.
 
 ```text
 DevServer: 1.0.5
-LiaScript: 0.9.19
+LiaScript: 0.9.20
 ```
 
 ### `--input`
--- src/index.ts
+++ src/index.ts
@@ -30,7 +30,7 @@ function liascript() {
 
 if (argv.v || argv.version) {
   console.log("DevServer: 1.0.5");
-  console.log("LiaScript: 0.9.19");
+  console.log("LiaScript: 0.9.20");
   process.exit();
 }
 
