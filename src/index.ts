4966a3d1fa899a37c6244c7880f0c868e5d0a488
André Dietrich <andredietrich@web.de>
2022-01-24 20:34:45 +0100

    Upgrade to LiaScript 0.9.49

--- src/index.ts
+++ src/index.ts
@@ -30,7 +30,7 @@ function liascript() {
 
 if (argv.v || argv.version) {
   console.log('DevServer: 1.0.5')
-  console.log('LiaScript: 0.9.47')
+  console.log('LiaScript: 0.9.49')
   process.exit()
 }
 
