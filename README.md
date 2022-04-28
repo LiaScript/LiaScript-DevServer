4b59183353f6b7cfdff9416411c5d6efd801e905
André Dietrich <andredietrich@web.de>
2022-04-28 15:52:43 +0200

    upgrade to version 1.0.10

--- README.md
+++ README.md
@@ -78,7 +78,7 @@ updated separately, we use two different version, one for the devServer and one
 for LiaScript.
 
 ```text
-DevServer: 1.0.9
+DevServer: 1.0.10
 LiaScript: 0.10.8
 ```
 
--- src/index.ts
+++ src/index.ts
@@ -13,7 +13,7 @@ function liascript() {
 }
 
 if (argv.v || argv.version) {
-  console.log('DevServer: 1.0.9')
+  console.log('DevServer: 1.0.10')
   console.log('LiaScript: 0.10.8')
   process.exit()
 }
