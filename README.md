385197325a5191e48019ac8262aba2ddf7be8b73
André Dietrich <andredietrich@web.de>
2022-04-29 08:37:10 +0200

    Version bump to 1.0.14

--- README.md
+++ README.md
@@ -78,7 +78,7 @@ updated separately, we use two different version, one for the devServer and one
 for LiaScript.
 
 ```text
-DevServer: 1.0.12
+DevServer: 1.0.14
 LiaScript: 0.10.8
 ```
 
--- src/index.ts
+++ src/index.ts
@@ -13,7 +13,7 @@ function liascript() {
 }
 
 if (argv.v || argv.version) {
-  console.log('DevServer: 1.0.12')
+  console.log('DevServer: 1.0.14')
   console.log('LiaScript: 0.10.8')
   process.exit()
 }
