412fa9b172fb2f865d93289ec4a54ec6c8a744ef
André Dietrich <andredietrich@web.de>
2024-10-29 12:30:46 +0100

    Upgrade to LiaScript 0.16.4

--- README.md
+++ README.md
@@ -84,8 +84,8 @@ updated separately, we use two different version, one for the devServer and one
 for LiaScript.
 
 ```text
-DevServer: 1.1.28
-LiaScript: 0.16.3
+DevServer: 1.1.29
+LiaScript: 0.16.4
 ```
 
 ### `--input`
--- src/index.ts
+++ src/index.ts
@@ -13,8 +13,8 @@ function liascript() {
 }
 
 if (argv.v || argv.version) {
-  console.log('DevServer: 1.1.28')
-  console.log('LiaScript: 0.16.3')
+  console.log('DevServer: 1.1.29')
+  console.log('LiaScript: 0.16.4')
   process.exit()
 }
 
